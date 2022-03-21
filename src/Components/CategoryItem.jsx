ca8edf5cf21d194d908caa5f9339dd85c42c4a70
Ayda Hadadian <89356581+aydian-dev@users.noreply.github.com>
2022-03-21 20:37:10 +0330

    Update CategoryItem.jsx

--- src/Components/CategoryItem.jsx
+++ src/Components/CategoryItem.jsx
@@ -3,38 +3,42 @@ import styled from 'styled-components'
 import {XS,SM} from "../responsive"
 
 
+const Info = styled.div`
+
+position: absolute;
+bottom:0;
+display: flex;
+justify-content: center;
+align-items: center;
+z-index: 3;
+width: 100%;
+background-color: #3a3a3a68;
+height:15%;
+transition: all .1s ease-in;
+background-color:${(props)=>props.bc}
+`
+
 const Container = styled.div`
 flex-grow:1;
-height:260px;
-margin: 8px;
+height: 42vh;
+width:20%;
+margin: 5px;
 position: relative;
 border-radius: 4px;
 overflow: hidden;
-background-color:${(props)=>props.bc};
-/* padding-left: 15px; */
-display:flex;
-justify-content:${(props)=>props.type === "left" ? "start" : "end"};
-${SM({width:"50%",height:"30vh"})};
-${XS({display:"none"})}
-::after{
-    content:"";
-    width:100%;
-    height:100%;
-    top:0;
-    left:0;
-    background-color:#000;
-    opacity:.1;
-    z-index:1;
-    position:absolute;
-    /* ${XS({opacity:".3"})} */
-}
-
+${SM({width:"30%"})}
+${XS({width:"90%"})}
 `
 const Wrapper = styled.div`
-height:100%;
-position:relative;
-${SM({position:"static"})}
 
+height:100%;
+display: flex;
+align-items: center;
+justify-content: center;
+cursor: pointer;
+:hover ${Info}{
+    height:100%;
+}
 
 `
 
@@ -42,67 +46,47 @@ const Image = styled.img`
 width: 100%;
 height: 100%;
 object-fit: cover;
-
-
-`
-
-const Info = styled.div`
-
-position: absolute;
-top:35px;
-right:${(props)=>props.type === "left" && "0"};
-left:${(props)=>props.type !== "left" && "10px"};
-width: ${(props)=>props.type === "left" ? "45%" : "50%"};
-flex-direction: column;
-
-
+position:relative;
+::after{
+    z-index: 1;
+    content: "";
+    width:100%;
+    height:100%;
+    position:absolute;
+    top:0;
+    left:0;
+    background-color:#000;
+    opacity:.3;
+    z-index:10;
+}
 `
 
 const Title = styled.h2`
-font-size: 1.9rem;
-font-weight: 600;
-z-index:4;
-${SM({fontSize:"1.8rem",fontWeight:"500"})}
 
-
-`
-const Text = styled.h5`
-font-weight: 500;
-z-index:4;
-padding-top: 11px;
-
-
-`
-const Btn = styled.button`
-padding: 10px;
-position:absolute;
-right:0;
-bottom:0;
-border: none;
-background:none;
-color:#000000;
-text-decoration:underline;
-  cursor: pointer;
+color: #ffffff;
+font-size: 1.1em;
 font-weight: 700;
+z-index:4;
+
+${SM({fontSize:"1.2rem",fontWeight:"500"})}
 
 `
 const CategoryItem = ({item}) => {
     return (
-        <Container bc={item.bc && item.bc} type={item.id===1 && "left"}>
-          
+        <Container bc={item.bc && item.bc}>
+            <Link to="/products">
                 <Wrapper>
 
+                  
                      <Image src={item.img} />
-                   
-           
-            <Info type={item.id===1 && "left"}>
+                  
+            <Info>
                 <Title>{item.title}</Title>
-                <Text>{item.desc}</Text>
-               
+         
             </Info>
             </Wrapper>
-         
-            <Btn>  <Link to="/products">SHOP NOW</Link></Btn>
+            </Link>
+
             
         </Container>
     )
